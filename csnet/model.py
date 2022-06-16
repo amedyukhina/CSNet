fbddb5dede39f674e9b6354862d44bcbc6dc67e8
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-06-16 15:16:20 -0500

    fix warnings

--- csnet/model.py
+++ csnet/model.py
@@ -26,7 +26,7 @@ def initialize_weights(*models):
     for model in models:
         for m in model.modules():
             if isinstance(m, nn.Conv3d) or isinstance(m, nn.Linear):
-                nn.init.kaiming_normal(m.weight)
+                nn.init.kaiming_normal_(m.weight)
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm3d):
