2b068c6fa22099de8b576a6ed51edd8c0b72e84a
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-11 15:46:25 -0500

    optimized training script

--- scripts/train.py
+++ scripts/train.py
@@ -1,26 +1,41 @@
 import argparse
+import json
 import os
 
+import numpy as np
 import wandb
+from monai.data import CacheDataset, DataLoader
+from monai.losses import DiceLoss
+from monai.metrics import DiceMetric
+from monai.networks.layers import Norm
+from monai.networks.nets import UNet
 
-from csnet.train import train
+from csnet.models.csnet import CSNet
+from csnet.models.csnet_orig import CSNetOrig
+from csnet.transforms.default import get_default_train_transforms
+from csnet.utils import get_data_dict, get_model_name
+from csnet.utils.train import train
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-tg', '--dir-train-gt', type=str,
-                        help='Directory with ground truth labels for training', required=True)
-    parser.add_argument('-ti', '--dir-train-img', type=str,
-                        help='Directory with input images for training', required=True)
-    parser.add_argument('-vg', '--dir-val-gt', type=str,
-                        help='Directory with ground truth labels for validation', required=True)
-    parser.add_argument('-vi', '--dir-val-img', type=str,
-                        help='Directory with input images for validation', required=True)
-    parser.add_argument('-m', '--model-path', type=str,
+    parser.add_argument('-d', '--data-dir', type=str,
+                        help='Directory with the data (training, validation, test)', required=True)
+    parser.add_argument('-t', '--train-dirname', type=str, default='train',
+                        help='Subdirectory within "data-dir" to use for training')
+    parser.add_argument('-v', '--val-dirname', type=str, default='val',
+                        help='Subdirectory within "data-dir" to use for validation')
+    parser.add_argument('-im', '--img-dirname', type=str, default='img',
+                        help='Subdirectory within "data-dir/train-dir" with input images')
+    parser.add_argument('-gt', '--gt-dirname', type=str, default='gt',
+                        help='Subdirectory within "data-dir/train-dir" with ground truth predictions')
+    parser.add_argument('-m', '--model', type=str, default='unet',
+                        help='Model type to use for prediction, one of ["unet", "csnet", "csnet_orig"]')
+    parser.add_argument('-c', '--n-channels', type=str, default="16,32,64,128",
+                        help='Number of channels in each UNet layers, separated by ","')
+    parser.add_argument('-r', '--num-res-units', type=int, default=1,
+                        help='Number of residual units in each block of Unet and CSNet')
+    parser.add_argument('-mp', '--model-path', type=str,
                         help='Directory for model checkpoints', default='model')
-    parser.add_argument('-s', '--snapshot', type=int, default=5,
-                        help='Save model snapshot every s epochs')
-    parser.add_argument('-v', '--val-step', type=int, default=5,
-                        help='Calculate validation accuracy every v epochs')
     parser.add_argument('-e', '--epochs', type=int, default=20,
                         help='Number of training epochs')
     parser.add_argument('-b', '--batch-size', type=int, default=2,
@@ -32,20 +47,18 @@ if __name__ == '__main__':
                         help='Factor parameter for ReduceOnPlateau learning rate scheduler')
     parser.add_argument('-p', '--patience', type=int, default=10,
                         help='Patience parameter for ReduceOnPlateau learning rate scheduler')
-    parser.add_argument('-ww', '--wce-loss-weight', type=float, default=0.6,
-                        help='Weight for the weighted cross entropy component of the loss')
-    parser.add_argument('-dw', '--dice-loss-weight', type=float, default=0.4,
-                        help='Weight for the dice component of the loss')
     parser.add_argument('-pr', '--wandb-project', type=str, default='',
                         help='wandb project name')
     parser.add_argument('-log', '--log-progress', action='store_true')
 
     config = parser.parse_args()
+    config.n_channels = np.int_(config.n_channels.split(','))
 
     print('\nThe following are the parameters that will be used:')
     print(vars(config))
     print('\n')
 
+    # Initialize wandb project
     if config.log_progress:
         with open('/home/amedyukh/.wandb_api_key') as f:
             key = f.read()
@@ -54,6 +67,64 @@ if __name__ == '__main__':
         os.environ['WANDB_MODE'] = 'offline'
 
     wandb.init(project=config.wandb_project, config=vars(config))
-    train(config.dir_train_img, config.dir_train_gt,
-          config.dir_val_img, config.dir_val_gt, config, config.log_progress)
+
+    # Update model path
+    config.model_path = os.path.join(config.model_path, get_model_name(config.log_progress))
+
+    # Save training parameters
+    os.makedirs(config['model_path'], exist_ok=True)
+    with open(os.path.join(config['model_path'], 'config.json'), 'w') as f:
+        json.dump(vars(config), f, indent=4)
+
+    # Setup model, loss, and metric
+    if config.model.lower() == 'unet':
+        net = UNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=2,
+            channels=config.model_channels,
+            strides=(2,) * (len(config.model_channels) - 1),
+            num_res_units=config.num_residual_units,
+            norm=Norm.BATCH,
+        )
+    elif config.model.lower() == 'csnet':
+        net = CSNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=2,
+            channels=config.model_channels,
+            strides=(2,) * (len(config.model_channels) - 1),
+            num_res_units=config.num_residual_units,
+            norm=Norm.BATCH,
+        )
+    elif config.model.lower() == 'csnet_orig':
+        net = CSNetOrig(2, 1)
+    else:
+        raise NotImplementedError(
+            rf'{config.model} is an invalid model; must be one of ["unet", "csnet", "csnet_orig"]')
+
+    loss_function = DiceLoss(to_onehot_y=True, softmax=True)
+    dice_metric = DiceMetric(include_background=False, reduction="mean")
+
+    # Setup data loaders
+    
+    # Train and validation transforms
+    train_tr, val_tr = get_default_train_transforms(roi_size=config.roi_size)
+
+    # Training and validation file lists
+    train_files = get_data_dict(os.path.join(config.data_dir, config.train_dirname, config.img_dirname),
+                                os.path.join(config.data_dir, config.train_dirname, config.gt_dirname))
+    val_files = get_data_dict(os.path.join(config.data_dir, config.val_dirname, config.img_dirname),
+                              os.path.join(config.data_dir, config.val_dirname, config.gt_dirname))
+
+    # Dataset and dataloader for training
+    tr_ds = CacheDataset(data=train_files, transform=train_tr, cache_rate=1, num_workers=2 * config.batch_size)
+    train_dl = DataLoader(tr_ds, batch_size=config.batch_size, shuffle=True, num_workers=2 * config.batch_size)
+
+    # Dataset and dataloader for validation
+    val_ds = CacheDataset(data=val_files, transform=val_tr, cache_rate=1.0, num_workers=2 * config.batch_size)
+    val_dl = DataLoader(val_ds, batch_size=config.batch_size, num_workers=2 * config.batch_size)
+
+    # train
+    train(train_dl, val_dl, net, loss_function, dice_metric, config, log_tensorboard=True)
     wandb.finish()
