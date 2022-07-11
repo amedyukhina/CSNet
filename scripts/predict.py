4b2dd0ea56021f21a34ec04ca5d686b5ec1c8547
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-11 16:23:06 -0500

    optimize prediction script

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,22 +1,19 @@
 import argparse
+import json
 import os
 
-import numpy as np
 import torch
-from skimage import io
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+from monai.networks.layers import Norm
+from monai.networks.nets import UNet
 
-from csnet.dataset import CS_Dataset
-from csnet.utils import get_device
+from csnet.models.csnet import CSNet
+from csnet.models.csnet_orig import CSNetOrig
+from csnet.utils.predict import predict
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-dir', type=str,
                         help='Input directory with images to predict', required=True)
-    parser.add_argument('-r', '--ref-dir', type=str,
-                        help='Reference directory with the ground truth, optional',
-                        default=None)
     parser.add_argument('-o', '--output-dir', type=str,
                         help='Directory to save predicted images', required=True)
     parser.add_argument('-m', '--model-path', type=str,
@@ -25,37 +22,43 @@ if __name__ == '__main__':
                         help='Batch size')
 
     args = parser.parse_args()
-    if args.ref_dir is None:
-        args.ref_dir = args.input_dir
 
     print('\nThe following are the parameters that will be used:')
     print(vars(args))
     print('\n')
 
-    ds = CS_Dataset(args.input_dir, args.ref_dir)
-    dl = DataLoader(ds, batch_size=args.batch_size, num_workers=args.batch_size, shuffle=False)
-    device = get_device()
-    net = torch.load(args.model_path).to(device)
+    # Setup and load model
+    with open(os.path.join(os.path.dirname(args.model_path), 'config.json')) as f:
+        config = json.load(f)
+    config = argparse.Namespace(**config)
 
-    imgs = []
-    lbls = []
-    predictions = []
+    if config.model.lower() == 'unet':
+        net = UNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=2,
+            channels=config.n_channels,
+            strides=(2,) * (len(config.n_channels) - 1),
+            num_res_units=config.num_res_units,
+            norm=Norm.BATCH,
+        )
+    elif config.model.lower() == 'csnet':
+        net = CSNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=2,
+            channels=config.n_channels,
+            strides=(2,) * (len(config.n_channels) - 1),
+            num_res_units=config.num_res_units,
+            norm=Norm.BATCH,
+        )
+    elif config.model.lower() == 'csnet_orig':
+        net = CSNetOrig(2, 1)
+    else:
+        raise NotImplementedError(
+            rf'{config.model} is an invalid model; must be one of ["unet", "csnet", "csnet_orig"]')
 
-    with torch.no_grad():
-        net.eval()
-        for idx, batch in enumerate(dl):
-            image = batch[0].to(device)
-            label = batch[1].to(device)
-            pred = net(image)
+    net.load_state_dict(torch.load(args.model_path))
 
-            imgs.append(image)
-            lbls.append(label)
-            predictions.append(torch.argmax(pred, dim=1))
-    imgs = torch.concat(imgs)
-    lbls = torch.concat(lbls)
-    predictions = torch.concat(predictions)
-
-    os.makedirs(args.output_dir, exist_ok=True)
-    for pred, fn in zip(tqdm(predictions), ds.image_fns):
-        io.imsave(fn.replace(args.input_dir.rstrip('/'), args.output_dir.rstrip('/')),
-                  (pred.cpu().numpy() * 255).astype(np.uint8))
+    image, predicted = predict(args.input_dir, args.output_dir, net,
+                               roi_size=config.roi_size, return_last=True, batch_size=args.batch_size)
