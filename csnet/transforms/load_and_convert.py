11a42be7d02e70d2e293ef313c4cbf5d777d4ff9
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-13 14:40:24 -0500

    bug fix

--- csnet/transforms/load_and_convert.py
+++ csnet/transforms/load_and_convert.py
@@ -12,7 +12,7 @@ def get_LoadAndConvertTransform(image_key='image', label_key='label'):
         keys = [image_key, label_key]
         scale = Compose([
             ScaleIntensityRanged(keys=[image_key], a_min=0, a_max=255, b_min=0.0, b_max=1.0, clip=True),
-            ScaleIntensityRanged(keys=[label_key], a_min=-0, a_max=255, b_min=0, b_max=1, clip=True),
+            ScaleIntensityRanged(keys=[label_key], a_min=0, a_max=255, b_min=0, b_max=1, clip=True),
         ])
     else:
         keys = [image_key]
